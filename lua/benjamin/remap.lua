3f18cf3d399548fadc73ddf024771a7bf86850bf
Benjamín Cordero <ingbenjamincordero@gmail.com>
2023-05-06 11:10:03 -0400

    tabby for tab styles

--- lua/benjamin/remap.lua
+++ lua/benjamin/remap.lua
@@ -12,23 +12,30 @@ vim.keymap.set({ 'n', 'v' }, '<M-z>', ':set wrap!<CR>', { silent = true })
 vim.keymap.set('v', '<Tab>', '>gv<CR>')
 vim.keymap.set('v', '<S-Tab>', '<gv<CR>')
 vim.keymap.set('n', '<ESC>', ':nohl<CR>', { silent = true })
-vim.keymap.set('n', '<Leader>j', ':vsplit<CR>')
-vim.keymap.set('n', '<Leader>h', ':split<CR>')
+vim.keymap.set('n', '<Leader>j', ':vsplit<CR>', opts)
+vim.keymap.set('n', '<Leader>h', ':split<CR>', opts)
 
 -- Terminal
 vim.keymap.set({ 'n', 'v' }, '<C-t>', ':split<CR>:ter<CR>:resize 15<CR>')
 
 -- Size windows
-vim.keymap.set('n', '<Leader>>', '10<C-w>>')
-vim.keymap.set('n', '<Leader><', '10<C-w><')
+vim.keymap.set('n', '<Leader>>', '10<C-w>>', opts)
+vim.keymap.set('n', '<Leader><', '10<C-w><', opts)
 
 -- editor
-vim.keymap.set('n', 'tp', ':tabprevious<CR>')
-vim.keymap.set('n', 'tn', ':tabnext<CR>')
-vim.keymap.set('n', 'tf', ':tabfirst<CR>')
-vim.keymap.set('n', 'tl', ':tablast<CR>')
-vim.keymap.set('n', '<TAB>', ':tabnext<CR>')
-vim.keymap.set('n', '<S-TAB>', ':tabprevious<CR>')
+vim.keymap.set('n', 'tp', ':tabprevious<CR>', opts)
+vim.keymap.set('n', 'tn', ':tabnext<CR>', opts)
+vim.keymap.set('n', 'tf', ':tabfirst<CR>', opts)
+vim.keymap.set('n', 'tl', ':tablast<CR>', opts)
+vim.keymap.set('n', '<TAB>', ':tabnext<CR>', opts)
+vim.keymap.set('n', '<S-TAB>', ':tabprevious<CR>', opts)
+
+vim.keymap.set('n', 'gt1', ':tabn1<CR>', opts)
+vim.keymap.set('n', 'gt2', ':tabn2<CR>', opts)
+vim.keymap.set('n', 'gt3', ':tabn3<CR>', opts)
+vim.keymap.set('n', 'gt4', ':tabn4<CR>', opts)
+vim.keymap.set('n', 'gt5', ':tabn5<CR>', opts)
+vim.keymap.set('n', 'gt6', ':tabn6<CR>', opts)
 
 vim.keymap.set('n', '<Leader>b', ':NvimTreeToggle<CR>', opts)
 vim.keymap.set({ 'n', 'v' }, '--', '<plug>NERDCommenterToggle', opts)
